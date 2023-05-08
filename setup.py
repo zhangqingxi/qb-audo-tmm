4af0a09bddd757a3d3473373e34c6ca9c00c6858
root <root@debian.debian>
2023-05-08 10:28:17 +0800

    Project Init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='qBittorrent Automatic Torrent Management',
+    author='Qasim',
+    license='MIT',
+)
